4a283ec553ca053fb83ad5c12caa8e616358790f
CaoTuan2005 <214935341+CaoTuan2005@users.noreply.github.com>
2025-11-14 11:42:22 +0700

    Create b1.sql

---
+++ bai1_SQL/b1.sql
@@ -0,0 +1,51 @@
+CREATE  DATABASE  IF NOT EXISTS XEM_PHIM;
+CREATE TABLE the_loai(
+	id int PRIMARY KEY
+  	TenTheLoai varchar(50)
+),
+CREATE TABLE phim(
+	TenPhim varchar(50) PRIMARY KEY,
+    thoi luong int,
+    poster varchar,
+    Nam_Phat_Hanh  int,
+    Dien_Vien varchar(50),
+    Dao_Dien 
+),
+CREATE TABLE phim_dien_vien
+CREATE TABLE nguoi_dung(
+	user varchar (50) 
+	MatKhau varchar (50)
+	id varchar (10) PRIMARY KEY ,
+    taikhoan int
+    email varchar (50)
+    sdt varchar(10)
+    quyen_id int
+    ngaysinh datetime
+ ),
+ CREATE TABLE Quyen(
+     id int 
+     tenQuyen varchar (50)
+     
+ )
+phim_the_loai 
+	 theloai id 
+     
+CREATE TABLE quoc gia(
+	TEN QUOC GIA PRIMARY KEY
+    MaQuocGia
+),
+tap phim
+	id
+    so tap int(pk)
+    thoi_luong varchar 
+    trailer varchar
+    tieu_de varchar
+    
+   
+
+
+
+
+    
+
+    
